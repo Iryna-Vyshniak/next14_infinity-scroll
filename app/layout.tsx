33f836113c3f306b3aae9446303aa9c19ed00fa3
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-04 23:15:57 +0200

    extract AnimeCard rendering logic and use LoadMore component, add components hero and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { DM_Sans } from 'next/font/google';
 
 import './globals.css';
+import Hero from '@/components/Hero';
+import Footer from '@/components/Footer';
 
 const dmSans = DM_Sans({ subsets: ['latin'] });
 
@@ -14,7 +16,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={dmSans.className}>
-        <main className='max-w-7xl mx-auto bg-[#0F1117]'>{children}</main>
+        <main className='max-w-7xl mx-auto bg-[#0F1117]'>
+          <Hero />
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   );
